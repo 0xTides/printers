d69e7732b11e4c24dda5dc54804fd3f421fca10c
tides <71273682+0xTides@users.noreply.github.com>
2023-02-27 16:05:21 +0000

    Update HP-printer-install.sh

--- HP-printer-install.sh
+++ HP-printer-install.sh
@@ -7,6 +7,13 @@ echo "Deleting Printer:" $printer
 lpadmin -x $printer
 done
 
+# give rights to add printers
+/usr/bin/security authorizationdb write system.preferences.printing allow
+/usr/bin/security authorizationdb write system.print.operator allow
+/usr/sbin/dseditgroup -o edit -n /Local/Default -a everyone -t group lpadmin
+/usr/sbin/dseditgroup -o edit -n /Local/Default -a everyone -t group _lpadmin
+
+
 # Install PKG - HP printer drivers
 
 # Set constants
